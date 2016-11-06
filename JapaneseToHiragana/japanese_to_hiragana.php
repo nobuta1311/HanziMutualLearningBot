e3a46c63ed5b33711f9d3639899587187a48b48f
nobuta1311 <nobunobucarp@gmail.com>
2016-11-07 05:43:05 +0900

    修正

--- JapaneseToHiragana/japanese_to_hiragana.php
+++ JapaneseToHiragana/japanese_to_hiragana.php
@@ -17,7 +17,6 @@ $data = array(
 );
 $headers = array(
     'Content-Type: application/x-www-form-urlencoded',
-    'User-Agent: My User Agent 1.0',    //ユーザエージェントの指定
 );
 $options = array('http' => array(
     'method' => 'POST',
