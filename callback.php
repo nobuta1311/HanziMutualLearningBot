909d20d96102c02adbeae1b6c133be841f237f0a
yuji nobuke <yuji@nobuta.xyz>
2016-11-17 03:29:12 +0900

    update

--- callback.php
+++ callback.php
@@ -1,20 +1,20 @@
 <?php
 require_once './lineapikey.php';
 require_once __DIR__ . '/../line/vendor/autoload.php';
+require_once "./command_carousel.php";
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 use \LINE\LINEBot\MessageBuilder\MultiMessageBuilder as MultiMessageBuilder;
 //POST
-include "command_carousel.php";
 $input = file_get_contents('php://input');
 $json = json_decode($input);
 $event = $json->events[0];
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($lineaccesstoken);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $linechannelsecret]);
-
-$MessageBuilder = new MultiMessageBuilder();
+$MessageBuilder = null;
 
 //イベントタイプ判別
 if ("message" == $event->type) {            //一般的なメッセージ(文字・イメージ・音声・位置情報・スタンプ含む)
+    $MessageBuilder = new MultiMessageBuilder();	//メッセージ用意
     //テキストメッセージにはオウムで返す
     if ("text" == $event->message->type) {
 	$reserved = $event->message->text;
@@ -23,18 +23,27 @@ if ("message" == $event->type) {            //一般的なメッセージ(文字
 	$MessageBuilder_part =  new TextMessageBuilder($result[0]);
 	$MessageBuilder->add($MessageBuilder_part);
     } else {
-	$MessageBuilder = command_carousel();
+	$MessageBuilder_part = command_carousel();
+	syslog(LOG_EMERG, print_r($MessageBuilder_part, true));
+
+	$MessageBuilder->add($MessageBuilder_part);
     }
+} elseif("postback" == $event->type){
+   
 } elseif ("follow" == $event->type) {        //お友達追加時
-    $MessageBuilder = new TextMessageBuilder("よろしくー．");
+    $MessageBuilder = new TextMessageBuilder("友達追加ありがとうございます!");
 } elseif ("join" == $event->type) {           //グループに入ったときのイベント
-    $MessageBuilder = new TextMessageBuilder('ご招待ありがとうございます．');
-} else {
-    //なにもしない
+    $MessageBuilder= new TextMessageBuilder('グループご招待ありがとうございます．');
+} elseif("leave" == $event->type){
+						//退出させられた
+} elseif("unfollow" == $event->type){
+} else {					//ブロックされた
+    $MessageBuilder=new TextMessageBuilder("対応出来ません");
 }
 
-
-$response = $bot->replyMessage($event->replyToken, $MessageBuilder);
+if($MessageBuilder!=null){
+	$response = $bot->replyMessage($event->replyToken, $MessageBuilder);
+}
 //syslog(LOG_EMERG, print_r($event->replyToken, true));
 //syslog(LOG_EMERG, print_r($response, true));
 return;
--- command_carousel.php
+++ command_carousel.php
@@ -2,8 +2,10 @@
 use \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder as TemplateMessageBuilder;
 use \LINE\LINEBot\MessageBuilder\TemplateBuilder as TemplateBuilder;
 use \LINE\LINEBot\TemplateActionBuilder as TemplateActionBuilder;
+var_dump(command_carousel());
 function command_carousel(){
     $columns = []; // カルーセル型カラムを5つ追加する配列
+    $actions = [];
     //パラメータを順番に羅列
     $actions_message=["通常の参照","漢字１文字の参照","テストと参照","クイズを開始","学習状況画像","記録済み漢字一覧","簡体字繁体字相互変換","音声確認","フィードバック","ユーザ設定変更","発音記号種類変更","リセット"];//15個
     $actions_uri=["","","","","","","","","","","","","","",""];	//15個
@@ -14,7 +16,7 @@ function command_carousel(){
     	for($j=0;$j<3;$j++)
     	$actions[$j] = new TemplateActionBuilder\UriTemplateActionBuilder($actions_message[$i*3+$j], "https://nobuta.xyz");//$actions_uri[$i*3+$j] );
     // カルーセルのカラムを作成する
-    	$column = new TemplateBuilder\CarouselColumnTemplateBuilder($column_title[$i], $column_detail[$i], "https://nobuta.xyz/line/image/".$i.".png", $actions);
+    	$column = new TemplateBuilder\CarouselColumnTemplateBuilder($column_title[$i], $column_detail[$i], "https://nobuta.xyz/HanziMutualLearningBot/image/".$i.".png", $actions);
     	$columns[] = $column;
     }
 // カラムの配列を組み合わせてカルーセルを作成する
